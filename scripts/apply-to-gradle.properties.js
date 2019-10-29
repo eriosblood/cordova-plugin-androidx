ac149c34cc0f05ca38597fd1be2d5c26f3302f98
eriosblood <littlestone3@yahoo.com>
2019-10-29 09:48:13 -0400

    Update apply-to-gradle.properties.js

--- scripts/apply-to-gradle.properties.js
+++ scripts/apply-to-gradle.properties.js
@@ -1,7 +1,7 @@
 var PLUGIN_NAME = "cordova-plugin-androidx";
 var enableAndroidX = "android.useAndroidX=true";
 var enableJetifier = "android.enableJetifier=true";
-var javaHeapSpace = "org.gradle.jvmargs=-Xmx16384m -XX:MaxPermSize=4096m -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8";
+var javaHeapSpace = "org.gradle.jvmargs=-XX:MaxPermSize=1024m -Xms1024m -Xmx4096m -XX:ReservedCodeCacheSize=1024m";
 var daemon = "org.gradle.daemon=true";
 var parallel = "org.gradle.parallel=true";
 var onDemand = "org.gradle.configureondemand=true";
